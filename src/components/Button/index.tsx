d4bca3c3fdd337e4dd3597a786675544d2e53ca3
athena272 <guilhermera272@gmail.com>
2025-03-15 18:44:53 -0300

    Create index.tsx

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import styles from './Button.module.scss'
+
+type ButtonProps = {
+    title: string,
+    kind: string,
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    type: "button" | "submit" | "reset",
+}
+
+export default function Button({ title, kind, onClick, type }: ButtonProps) {
+    const generationClassByKind = () => {
+        if (kind === "secundary") return styles.secundary;
+        if (kind === "full") return styles.full;
+
+        return styles.primary;
+    };
+
+    return (
+        <button
+            type={type}
+            className={`${styles.button} ${generationClassByKind()}`}
+            onClick={onClick}
+        >
+            {title}
+        </button>
+    )
+}
