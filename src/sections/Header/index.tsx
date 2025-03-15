e501bbc888e3f82d265dada52d48e2462bb57f02
athena272 <guilhermera272@gmail.com>
2025-03-15 18:52:18 -0300

    Create index.tsx

---
+++ src/sections/Header/index.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from './images/choran-logo.webp'
+import styles from './Header.module.scss'
+import Button from '../../components/Button'
+import scrollTo from '@/utils/scrollTo'
+
+export default function Header() {
+    return (
+        <header className={styles.container}>
+            <div className={styles.logotipo}>
+                <Image src={logo} alt="Logo" />
+            </div>
+            <nav className={styles.menu}>
+                <Link href="/">PUV</Link>
+                <Link href="/">Formulário</Link>
+                <Link href="/">Solução</Link>
+            </nav>
+            <div className={styles.action}>
+                <Button title="Fale conosco" onClick={() => scrollTo("contat")} />
+            </div>
+        </header>
+    )
+}
