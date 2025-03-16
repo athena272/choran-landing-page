e9c02e764373772b13df32abc2317af4aa93ae85
athena272 <guilhermera272@gmail.com>
2025-03-16 09:21:21 -0300

    Create index.tsx

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes } from "react";
+import styles from "./input.module.scss";
+
+// Tipagem para as propriedades do Input
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+export default function Input({
+    type = "text", // Valor padrão para evitar undefined
+    placeholder,
+    pattern,
+    required,
+    onBlur,
+    name,
+    id,
+    onChange,
+    value,
+    className, // Permitir adicionar classes externas
+    ...rest // Captura outras props do input nativo
+}: InputProps) {
+    return (
+        <input
+            id={id}
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            pattern={pattern}
+            required={required}
+            className={`${styles.input} ${className || ""}`} // Permite adicionar classes adicionais
+            onBlur={onBlur}
+            onChange={onChange}
+            value={value}
+            {...rest} // Passa quaisquer outras propriedades adicionais
+        />
+    )
+}
