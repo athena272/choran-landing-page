e8544a689896d1c3ba70000f0f050fbf165ac4bc
athena272 <guilhermera272@gmail.com>
2025-03-15 21:43:29 -0300

    Update index.tsx

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,9 @@
-import stylse from './Loading.module.scss'
+import styles from './Loading.module.scss'
 
 export default function Loading() {
-
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.loading} />
+        </div>
+    )
 }
