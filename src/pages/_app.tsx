4782b934392db5e366b1c0ffe619cd6fa353bde1
athena272 <guilhermera272@gmail.com>
2025-03-16 15:11:54 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Choran Landing Page - A modern and interactive landing page built with Next.js and React." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
